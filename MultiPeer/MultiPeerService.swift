ed216abb1d7576a2ad4445946513664eec12b423
Maxime Moison <mmoison@yelp.com>
2018-07-25 19:15:42 -0700

    initial commit

---
+++ MultiPeer/MultiPeerService.swift
@@ -0,0 +1,145 @@
+//
+//  MultiPeerService.swift
+//  MultiPeerService
+//
+//  Created by Maxime Moison on 7/25/18.
+//  Copyright © 2018 Maxime Moison. All rights reserved.
+//
+
+import Foundation
+import MultipeerConnectivity
+
+
+protocol MultiPeerServiceDelegate {
+    
+    func connectedDevicesChanged(manager : MultiPeerService, connectedDevices: [String])
+    func didReceiveMessage(manager : MultiPeerService, message: String)
+    
+}
+
+class MultiPeerService: NSObject {
+    // Service type must be a unique string, at most 15 characters long
+    // and can contain only ASCII lowercase letters, numbers and hyphens.
+    private let MultiPeerServiceType = "max-multipeer"
+    
+    private let myPeerId = MCPeerID(displayName: UIDevice.current.name)
+    private let serviceAdvertiser : MCNearbyServiceAdvertiser
+    private let serviceBrowser : MCNearbyServiceBrowser
+    
+    lazy var session : MCSession = {
+        let session = MCSession(peer: self.myPeerId, securityIdentity: nil, encryptionPreference: .required)
+        session.delegate = self
+        return session
+    }()
+    
+    var delegate : MultiPeerServiceDelegate?
+    
+    override init() {
+        self.serviceAdvertiser = MCNearbyServiceAdvertiser(peer: myPeerId, discoveryInfo: nil, serviceType: MultiPeerServiceType)
+        self.serviceBrowser = MCNearbyServiceBrowser(peer: myPeerId, serviceType: MultiPeerServiceType)
+        
+        super.init()
+        
+        self.serviceAdvertiser.delegate = self
+        self.serviceAdvertiser.startAdvertisingPeer()
+        
+        self.serviceBrowser.delegate = self
+        self.serviceBrowser.startBrowsingForPeers()
+    }
+    
+    deinit {
+        self.serviceAdvertiser.stopAdvertisingPeer()
+        self.serviceBrowser.stopBrowsingForPeers()
+    }
+    
+    
+    func send(message : String) {
+        debugPrint("sendMessage: \(message) to \(session.connectedPeers.count) peers")
+        
+        if session.connectedPeers.count > 0 {
+            do {
+                try self.session.send(message.data(using: .utf8)!, toPeers: session.connectedPeers, with: .reliable)
+            }
+            catch let error {
+                NSLog("%@", "Error for sending: \(error)")
+            }
+        }
+        
+    }
+    
+    
+}
+
+
+
+
+
+
+extension MultiPeerService : MCNearbyServiceAdvertiserDelegate {
+    
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
+        debugPrint("didNotStartAdvertisingPeer: \(error)")
+    }
+    
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+        debugPrint("didReceiveInvitationFromPeer \(peerID)")
+        invitationHandler(true, self.session)
+    }
+    
+}
+
+
+
+
+
+extension MultiPeerService : MCSessionDelegate {
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        debugPrint("peer \(peerID) didChangeState: \(state.rawValue)")
+        self.delegate?.connectedDevicesChanged(manager: self, connectedDevices:
+            session.connectedPeers.map{$0.displayName})
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        debugPrint("didReceiveData: \(data)")
+        let str = String(data: data, encoding: .utf8)!
+        self.delegate?.didReceiveMessage(manager: self, message: "\(peerID.displayName): \(str)")
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        debugPrint("didReceiveStream")
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        debugPrint("didStartReceivingResourceWithName")
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        debugPrint("didFinishReceivingResourceWithName")
+    }
+    
+}
+
+
+
+
+
+
+
+extension MultiPeerService : MCNearbyServiceBrowserDelegate {
+    
+    func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
+        debugPrint("didNotStartBrowsingForPeers: \(error)")
+    }
+    
+    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
+        debugPrint("foundPeer: \(peerID)")
+        debugPrint("invitePeer: \(peerID)")
+        browser.invitePeer(peerID, to: self.session, withContext: nil, timeout: 10)
+    }
+    
+    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+        debugPrint("lostPeer: \(peerID)")
+    }
+    
+}
--- MultiPeer/ViewController.swift
+++ MultiPeer/ViewController.swift
@@ -9,17 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let multiPeerService = MultiPeerService()
 
+    @IBOutlet weak var connectionsLabel: UILabel!
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var broadcastLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        multiPeerService.delegate = self
+        // Add an observer to check on keyboard status
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: NSNotification.Name.UIKeyboardWillShow,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: NSNotification.Name.UIKeyboardWillHide,
+            object: nil
+        )
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func textFieldDidReturn(_ sender: UITextField) {
+        guard let msg = sender.text else { return }
+        multiPeerService.send(message: msg)
+        sender.text = ""
     }
-
-
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        // When keyboard shows, change inset in searchTable so that nothing is under keyboard
+        if let keyboardFrame: NSValue = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+            messageTextField.transform = CGAffineTransform(translationX: 0, y: -keyboardHeight)
+        }
+    }
+    @objc func keyboardWillHide(_ notification: Notification) {
+        messageTextField.transform = CGAffineTransform(translationX: 0, y: 0)
+    }
 }
 
+extension ViewController : MultiPeerServiceDelegate {
+    
+    func connectedDevicesChanged(manager: MultiPeerService, connectedDevices: [String]) {
+        OperationQueue.main.addOperation {
+            self.connectionsLabel.text = "\(connectedDevices.count)"
+        }
+    }
+    
+    func didReceiveMessage(manager : MultiPeerService, message: String) {
+        OperationQueue.main.addOperation {
+            self.broadcastLabel.text = message
+        }
+    }
+}
